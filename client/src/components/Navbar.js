9f189cd765c3410b52cd04a3b1400a055ff226e4
Scott <scrosehart@gmail.com>
2018-03-18 22:50:54 -0700

    added login/signup to nav...not working with database tho

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../css/navbar.css';
 import Home from './Home';
 import Discover from './Discover';
 import { UserProfile } from './UserProfile';
 import PublicProfile from './PublicProfile';
-import { OurTeam } from './OurTeam';
 import Login from './Login';
 import Signup from './Signup';
+import { OurTeam } from './OurTeam';
 
 class Nav extends Component {
   constructor(props) {
@@ -17,11 +18,13 @@ class Nav extends Component {
     return (
       <div>
         <h1>~~~~~~~~~~~NAV PLACEHOLDER section~~~~~~~~~~~~~~</h1>
-        <ul>
+        <ul className='nav-ul'>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/discover'>Discover</Link></li>
           <li><Link to='/profile'>User Profile</Link></li>  {/* placeholder so we can easily get to page */}
           <li><Link to='/publicprofile'>Public Profile</Link></li>  {/* placeholder so we can easily get to page */}
+          <li><Link to='/login'>Log In</Link></li>  {/* placeholder so we can easily get to page */}
+          <li><Link to='/signup'>Sign Up</Link></li>  {/* placeholder so we can easily get to page */}
           <li><Link to='/ourteam'>OurTeam</Link></li>
           <li><Link to='/'>Logout</Link></li>
         </ul>
