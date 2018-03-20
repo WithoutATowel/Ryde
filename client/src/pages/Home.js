93821c1c4778e9e8667a1de5fd1d04efbde43633
Brett <brettspencr@gmail.com>
2018-03-20 15:05:28 -0700

    More warning cleanup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,14 @@ import MiniSearch from '../components/MiniSearch';
 import '../css/home.css';
 
 class Home extends Component {
-  constructor(props) {
-    super(props)
-  }
+  // constructor(props) {
+  //   super(props)
+  // }
 
   render() {
     return (
       <div>
-        <img src="/img/home-hero.jpg" className="home-hero" alt="traffic-hero-image" />
+        <img src="/img/home-hero.jpg" className="home-hero" alt="traffic-hero" />
         <MiniSearch />
       </div>
 
