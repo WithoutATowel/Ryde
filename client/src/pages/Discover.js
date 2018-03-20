97d2e82a308a72987aa5d98692cf04b111a6704c
Sean Cesmat <sean.cesmat@gmail.com>
2018-03-20 10:24:02 -0700

    Added the discover filter sidebar for desktop and mobile

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -14,11 +14,20 @@ class Discover extends Component {
     return (
       <div className='discover-div'>
         <div className='row'>
-          <div className='col s12 m4'>
+          <ul id="discover-slide-out" className="side-nav fixed hide-on-large-only">
+            <BigSearch />
+          </ul>
+          <div className="discover-nav-btn-cont">
+            <a href="#" data-activates="discover-slide-out" className="discover-sidnav hide-on-large-only">
+              <i className="material-icons">filter_list</i>
+              Filter Results
+            </a>
+          </div>
+          <div className='col s12 m4 hide-on-med-and-down'>
             <h1 className='discover-h1'>Find a Ryde</h1>
             <BigSearch />
           </div>
-          <div className='list-box-div col s12 m8'>
+          <div className='list-box-div col s12 m12 l8'>
             <h1>Search Results</h1>
             <ListBox />
           </div>
