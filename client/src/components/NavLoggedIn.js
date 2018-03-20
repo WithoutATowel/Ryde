f4dd38b9a05309115d03ae09a95890866291b8a3
Scott <scrosehart@gmail.com>
2018-03-19 18:44:40 -0700

    working on pub and user profile pages. added wip-screenshot folder

--- client/src/components/NavLoggedIn.js
+++ client/src/components/NavLoggedIn.js
@@ -10,6 +10,10 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
+const mapStateToProps = state => {
+  return { user: state.user };
+}
+
 class ConnectedNavLoggedIn extends Component {
   constructor(props) {
     super(props)
@@ -20,14 +24,23 @@ class ConnectedNavLoggedIn extends Component {
     localStorage.removeItem('rydeAppToken');
   }
 
+  // componentDidMount() {
+  //   console.log('comp did mount: ', this.props.user._id)
+  // }
+
   render() {
     return (
       <div>
+
+        <li><Link to={'/profile/' + this.props.user._id}>Scott public test</Link></li>
+        <li><Link to='/publicprofile'>Sean public test</Link></li>
+        <li><Link to='/publicprofile'>Brant public test</Link></li>
+        <li><Link to='/publicprofile'>Brett public test</Link></li>
+
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/discover'>Discover</Link></li>
         <li><Link to='/myrydes'>My Rydes</Link></li>
-        <li><Link to='/profile'>User Profile</Link></li>  {/* placeholder so we can easily get to page */}
-        <li><Link to='/publicprofile'>Public Profile</Link></li>  {/* placeholder so we can easily get to page */}
+        <li><Link to={'/profile/' + this.props.user._id}>{this.props.user.name}'s Profile</Link></li>
         <li><Link to='/ourteam'>OurTeam</Link></li>
         <li><Link to='/' onClick={() => this.handleClick()}>Logout</Link></li>
       </div>
@@ -35,6 +48,6 @@ class ConnectedNavLoggedIn extends Component {
   }
 }
 
-const NavLoggedIn = connect(null, mapDispatchToProps)(ConnectedNavLoggedIn);
+const NavLoggedIn = connect(mapStateToProps, mapDispatchToProps)(ConnectedNavLoggedIn);
 
 export default NavLoggedIn;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,11 +15,10 @@ class Home extends Component {
         <MiniSearch />
         <h4>Links to user profiles PLACEHOLDER</h4>
         <ul>
-          <li><Link to='/publicprofile'>Home</Link></li>
-          <li><Link to='/publicprofile'>Scott</Link></li>
-          <li><Link to='/publicprofile'>Sean</Link></li>
-          <li><Link to='/publicprofile'>Brant</Link></li>
-          <li><Link to='/publicprofile'>Brett</Link></li>
+          {/* <li><Link to={'/profile/' + this.props.user._id}>Scott public test</Link></li>
+          <li><Link to='/publicprofile'>Sean public test</Link></li>
+          <li><Link to='/publicprofile'>Brant public test</Link></li>
+          <li><Link to='/publicprofile'>Brett public test</Link></li> */}
         </ul>
       </div>
 
