688d7c7e346a37433850f6d0972849c4e2744f40
Brett <brettspencr@gmail.com>
2018-03-23 13:55:33 -0700

    Hid PostARyde and MyDryves if the user is not a dryver

--- client/src/components/NavLoggedIn.js
+++ client/src/components/NavLoggedIn.js
@@ -21,18 +21,30 @@ class ConnectedNavLoggedIn extends Component {
   }
 
   render() {
-    // if (this.props.user.)
-    return (
-      <div className='nav-link-div'>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/discover'>Discover</Link></li>
-        <li><Link to='/postaryde'>Post a Ryde</Link></li>
-        <li><Link to='/myrydes'>My Rydes</Link></li>
-        <li><Link to={'/profile/' + this.props.user._id}>{this.props.user.name.match(/\S+/)}'s Profile</Link></li>
-        <li><Link to='/ourteam'>About</Link></li>
-        <li><Link to='/' onClick={() => this.handleClick()}>Logout</Link></li>
-      </div>
-    )
+    if (this.props.user.dryver) {
+      return (
+        <div className='nav-link-div'>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/discover'>Discover</Link></li>
+          <li><Link to='/postaryde'>Post a Ryde</Link></li>
+          <li><Link to='/myrydes'>My Rydes</Link></li>
+          <li><Link to={'/profile/' + this.props.user._id}>{this.props.user.name.match(/\S+/)}'s Profile</Link></li>
+          <li><Link to='/ourteam'>About</Link></li>
+          <li><Link to='/' onClick={() => this.handleClick()}>Logout</Link></li>
+        </div>
+      )
+    } else {
+      return (
+        <div className='nav-link-div'>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/discover'>Discover</Link></li>
+          <li><Link to='/myrydes'>My Rydes</Link></li>
+          <li><Link to={'/profile/' + this.props.user._id}>{this.props.user.name.match(/\S+/)}'s Profile</Link></li>
+          <li><Link to='/ourteam'>About</Link></li>
+          <li><Link to='/' onClick={() => this.handleClick()}>Logout</Link></li>
+        </div>
+      )
+    }
   }
 }
 
