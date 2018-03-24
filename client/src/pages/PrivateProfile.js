af6ff123b4eab0f2d69c3f5ea3e1e5caec9a517b
Brett <brettspencr@gmail.com>
2018-03-23 17:30:38 -0700

    styled profile page

--- client/src/pages/PrivateProfile.js
+++ client/src/pages/PrivateProfile.js
@@ -18,7 +18,7 @@ class PrivateProfile extends Component {
     let ryderRating = user.ryderRatingAvg
     let dryverRating = user.dryverRatingAvg
     let dryverOptions = user.dryver ? <NoLongerDryve userId={user._id} /> : <BecomeADryver userId={user._id} />;
-    let dryverSignupText = user.dryver ? 'No longer want to dryve?' : 'Become a Dryver';
+    let dryverSignupText = user.dryver ? 'Currently enrolled to dryve:' : 'Become a Dryver:';
     if (user) {
       return (
         <div id="private-profile" className="container">
@@ -28,43 +28,50 @@ class PrivateProfile extends Component {
           {/* <Modal header='Delete Yourself!' id='deleteuser-modal'>
             <DeleteUser />
           </Modal> */}
-          <h2 className='user-profile-h1'>{user.name}'s Profile Page</h2>
-          <div className='row profile-ratings-summary'>
+          <h2 className='user-profile-h1'>{user.name}'s Profile</h2>
+          <div className='row profile-card'>
             <div className='col m4'>
               <div className='pic-circle center-align'>
                 <img src='https://www.placecage.com/c/185/230' alt='profile' />
               </div>
             </div>
-            {/*<a href='#update-profile-modal' className="modal-trigger ">Update profile</a> */}
               <div className='profile-stats-column col m4'>
-                <h5>Ryder Stats</h5>
+                <h4>Ryder Stats</h4>
                 <ul>
-                  <li>Rating: {ryderRating > 0 ? ryderRating : 'no ratings yet'}</li>
-                  <li>Total Trips: {user.completedTrips.length > 0 ? user.completedTrips : 'no trips yet'}</li>
+                  <li><span className='bold'>Rating:</span> {ryderRating > 0 ? ryderRating : 'no ratings yet'}</li>
+                  <li><span className='bold'>Total Trips:</span> {user.completedTrips.length > 0 ? user.completedTrips : 'no trips yet'}</li>
                 </ul>
               </div>
               <div className='profile-stats-column col m4'>
-                <h5>Dryver Stats</h5>
+                <h4>Dryver Stats</h4>
                 <ul>
-                  <li>Dryver Rating: {dryverRating > 0 ? dryverRating : 'no ratings yet'}</li>
-                  <li>Total Dryves: {user.completedDryves.length > 0 ? user.completedDryves : 'no trips yet'}</li>
+                  <li><span className='bold'>Dryver Rating:</span> {dryverRating > 0 ? dryverRating : 'no ratings yet'}</li>
+                  <li><span className='bold'>Total Dryves:</span> {user.completedDryves.length > 0 ? user.completedDryves : 'no trips yet'}</li>
                 </ul>
               </div>
           </div>
+          <h3>Details</h3>
+          <div className='row profile-card'>
+            <UpdateProfile />
+          </div>
+          <h3>Dryver Settings</h3>
+          <div className='row profile-card'>
+            <div>
               <h5>{dryverSignupText}</h5>
               {dryverOptions}
-             {/* 
-              <br />
+            </div>
+          </div>
+           {/* 
+            <br />
 
-              <div>
-                <Modal
-                	header='Delete Yourself!'
-                  trigger={<Button>Delete</Button>}>
-                	<DeleteUser />
-                </Modal>
-              </div>
-              */}
-          
+            <div>
+              <Modal
+              	header='Delete Yourself!'
+                trigger={<Button>Delete</Button>}>
+              	<DeleteUser />
+              </Modal>
+            </div>
+            */}
         </div>
       )
     } else {
